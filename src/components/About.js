0cb8dac2671e784ebfea4e40ec8ea7ed3845dfc0
Arkin Zat <arkinzt@gmail.com>
2024-06-10 16:31:48 +0300

    BurgerProject
    Burger menüleri ve hakkımzda, iletişim gibi bölümler oluşturuldu

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import photos from '../assets/Photosss.png'
+import '../styles/About.css'
+
+export const About = () => {
+  return (
+    <div className='about'>
+      <div 
+        className='aboutTop' 
+        style={{backgroundImage: `url(${photos})`}}
+        ></div>
+      <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+      </div>
+    </div>
+  )
+}
---
+++ src/helpers/Data.js
@@ -0,0 +1,83 @@
+
+import React from 'react'
+import BurgerOne from '../assets/BurgerPhotoOne.png'
+import BurgerTwo from '../assets/BurgerPhotoTwo.png'
+import BurgerThree from '../assets/BurgerPhotoThree.png'
+
+export const Data = [
+    {
+        name: 'Efso Burger',
+        image: BurgerOne,
+        content: '150 gr kasap Burger',
+        price: 250,
+    },
+    {
+        name: 'Super Burger',
+        image: BurgerTwo,
+        content: '200 gr kasap Burger',
+        price: 300,
+    },
+    {
+        name: 'Ultra Super Burger',
+        image: BurgerThree,
+        content: '250 gr kasap Burger',
+        price: 350,
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerOne,
+        content: '150 gr kasap Burger',
+        price: 250,
+    },
+    {
+        name: 'Super Burger',
+        image: BurgerTwo,
+        content: '200 gr kasap Burger',
+        price: 300,
+    },
+    {
+        name: 'Ultra Super Burger',
+        image: BurgerThree,
+        content: '250 gr kasap Burger',
+        price: 350,
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerOne,
+        content: '150 gr kasap Burger',
+        price: 250,
+    },
+    {
+        name: 'Super Burger',
+        image: BurgerTwo,
+        content: '200 gr kasap Burger',
+        price: 300,
+    },
+    {
+        name: 'Ultra Super Burger',
+        image: BurgerThree,
+        content: '250 gr kasap Burger',
+        price: 350,
+    },
+    {
+        name: 'Efso Burger',
+        image: BurgerOne,
+        content: '150 gr kasap Burger',
+        price: 250,
+    },
+    {
+        name: 'Super Burger',
+        image: BurgerTwo,
+        content: '200 gr kasap Burger',
+        price: 300,
+    },
+    {
+        name: 'Ultra Super Burger',
+        image: BurgerThree,
+        content: '250 gr kasap Burger',
+        price: 350,
+    },
+
+];
+
+
